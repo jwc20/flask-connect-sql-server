61104ff58dd55dc26e1a52d8cba91f5908dcdfc1
cjw <choi.jae100@gmail.com>
2024-04-13 14:52:06 +0000

    Add script to connect to postgresql and use nixos

--- postgresql_connect.py
+++ postgresql_connect.py
@@ -1,5 +1,27 @@
-import dotenv
+from dotenv import load_dotenv
+import psycopg2
+import os
 
-import os 
+load_dotenv()
 
+DATABASE = os.getenv('DATABASE')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+
+    
+try:
+    conn = psycopg2.connect(
+    database=DATABASE,
+    user=DATABASE_USERNAME,
+    password=DATABASE_PASSWORD)
+
+    print(conn)
+    cur = conn.cursor()
+
+    cur.execute('select * from clients')
+    rows = cur.fetchall()
+    print(rows)
+
+except:
+    print('Error')
 
